22b957ab267489fbac6996cb89fdab96bae1192e
Daniel Quero <danielquero95@gmail.com>
2023-11-16 18:07:24 +0100

    Added about me section

--- src/ui/views/home/Home.vue
+++ src/ui/views/home/Home.vue
@@ -1,15 +1,22 @@
 <template lang="pug">
 div.home
   full-header
+  about-me
+
+  rocket
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-facing-decorator'
 import FullHeader from './sections/fullHeader/FullHeader.vue'
+import AboutMe from './sections/aboutMe/AboutMe.vue'
+import Rocket from '@/ui/components/rocket/Rocket.vue'
 
 @Component({
 	components: {
 		FullHeader,
+		AboutMe,
+		Rocket,
 	},
 })
 export default class HomeView extends Vue {
