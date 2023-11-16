60f2eadfc0df6665fe34e63d1286ed4b4ff23ac3
Daniel Quero <danielquero95@gmail.com>
2023-11-16 17:43:55 +0100

    Created app layout and added favicon

--- src/ui/app/layout/topBar/topBar.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Vue } from 'vue-facing-decorator'
-import { RouterLink } from 'vue-router'
-
-@Component({
-	components: {
-		RouterLink,
-	},
-})
-export default class TopBar extends Vue {}
--- src/ui/app/layout/topBar/topBar.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-header
-  nav
-    router-link.app-title(to="/") Movie Showtimes
-</template>
-
-<script lang="ts" src="./topBar.ts"></script>
-
-<style lang="sass" scoped src="./topBar.sass"></style>
